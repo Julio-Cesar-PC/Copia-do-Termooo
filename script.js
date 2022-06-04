a11006fee96efde60d4125c28f2bf71827b5d884
Julio Cesar Polmann <juliocesarpolmann@gmail.com>
2022-06-04 00:39:07 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,68 @@
+import { PALAVRAS } from './palavras.js';
+
+const board = document.querySelector('#board');
+const keys = document.querySelectorAll('#keyboard-container button');
+
+const omreT = {
+    palavraDoDia: "omret",
+}
+
+
+let vetTentativa = [];
+let tentativa = 0;
+let point = 0;
+criaBoard();
+keyboardListener();
+
+
+function criaBoard () {
+    let id = 0;
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement('div');
+        row.id = 'row-' + i;
+        row.classList.add('row');
+        board.appendChild(row);
+        for (let j = 0; j < 5; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.classList.add("animate__animated");
+            square.setAttribute("id", id++);
+            row.appendChild(square);
+        }
+        
+    }
+}
+
+function keyboardListener() {
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+            //console.log(letter);
+      
+            if (letter === "enter") {
+              //handleSubmitWord();
+              return;
+            }
+      
+            if (letter === "del") {
+              //handleDeleteLetter();
+              return;
+            }
+      
+            updateGuessedWords(letter);
+        };
+    }
+}
+
+function updateGuessedWords(letter) {
+    if (point != 30) {
+        const pointer = document.getElementById(`${point}`);
+        pointer.innerHTML = letter;
+        point++;
+    }
+}
+
+
+
+
+
